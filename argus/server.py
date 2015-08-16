c05511ea241615a04dc413ab3474a9fc17571abc
Raphael Kargon <raphael.kargon@gmail.com>
2015-08-16 19:35:29 -0400

    added init for argus package

--- argus/server.py
+++ argus/server.py
@@ -3,17 +3,24 @@ Code for running the server.
 """
 
 from flask import Flask
+import sys
 from argus import Argus
 
 app = Flask(__name__, static_url_path="/static")
 argus = Argus()
 
+
 @app.route('/')
 def serve_ui():
-    """
-    Serve the ui's html.
+    """ Serve the ui's html.
     """
     return app.send_static_file('index.html')
 
-if __name__ == '__main__':
+
+def main(argv):
+    """ Parse arguments, and run the server.
+    """
     app.run()
+
+if __name__ == '__main__':
+    main(sys.argv)
